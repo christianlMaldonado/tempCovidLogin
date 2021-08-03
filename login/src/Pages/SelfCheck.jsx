556ff3495e7d3fe945f94180a50082fb0ea6fcbb
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-03 11:53:40 -0700

    adding controlled form components

--- login/src/Pages/SelfCheck.jsx
+++ login/src/Pages/SelfCheck.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import style from './SelfCheck.css'
+// import { GiTwinShell } from 'react-icons/gi';
+import './SelfCheck.css';
+import Component from './Form';
 
 function SelfCheck() {
+  
   return (
     <div className='selfCheck'>
         <h1 className="selfCheckTitle"> COUNTY OF MONTEREY </h1>
-        <h1 className="selfCheckTitle"> EMPLOYEE COVID-19 SELD SCREENING ASSESSMENT FORM </h1> 
+        <h1 className="selfCheckTitle"> EMPLOYEE COVID-19 SELF SCREENING ASSESSMENT FORM </h1> 
         <p className="selfCheckoutBody"> 
             Pursuant to the Human Resources Emergency Response Manual Section 21 Temperature Testing
             and Self-Screening Assessment for new Symptoms Associated with COVID-19, County employees
@@ -15,6 +18,10 @@ function SelfCheck() {
             provide this self-screening assessment form to your supervisor/manager.</b>
         </p>
 
+      <Component/>
+
+      <p> <b> <u> Employee Temperature Assessment </u></b></p>
+      <p> I have been tested with a thermometer and my temperature is less than 100.4 degrees Fahrenheit (Check the applicable answer): </p>
 
         
     </div>
