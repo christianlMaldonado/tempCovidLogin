bc3b1662eb028342f1ef429af50effe3e1bb5955
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-11 14:18:20 -0700

    added rest of form

--- login/src/Pages/NameForm.jsx
+++ login/src/Pages/NameForm.jsx
@@ -4,16 +4,16 @@ import './NameForm.css';
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: 'test', email: 'test 2' };
+    this.state = { text: 'test', email: 'atest@test', number: '7357' };
   }
 
   handleChange = (e) => {
-    this.setState({ name: e.target.text, email: e.target.email }
+    this.setState({ name: e.target.text, email: e.target.email, number: e.target.number }
     )
   }
 
   handleSubmit = (e) => {
-    alert('Text was just submitted: ' + this.state.email + this.state.email)
+    alert('Text was just submitted: ' + this.state.email + this.state.email + this.state.number)
     e.preventDefault()
   }
 
@@ -69,8 +69,43 @@ class NameForm extends React.Component {
           </p> 
 
           <p>
-              2. Are you experiencing any of these <b><u> NEW </u></b> symptoms <b> <u> NOT </u></b> associated with any other health condition? 
+              2. Are you experiencing any of these <b><u> NEW </u></b> symptoms <b> <u> NOT </u></b>
+               associated with any other health condition? (check all that apply)
           </p>
+          <select> 
+              <option selected value=""> </option>
+                <option value="Congestion"> Congestion or runny nose </option>
+                console.log(value);
+                <option value="Cough">Cough, shortness of breath or difficulty breathing </option>
+                <option value="Nausea">Nasuea, vomitting or diarrhea</option>
+                <option value="Fatigue">Fatigue</option>
+                <option value="Fever">Fever or chills</option>
+                <option value="Headache">Headache, muscle or body aches</option>
+                <option value="lossTaste">Loss of taste or smell</option>
+                <option value="Throat">Sore throat</option>
+                <option value="NoSymptoms">I am not Experiencing any of the listed symptoms</option>
+            </select>  
+
+            <p>
+                If you checked the box for any of the listed symptoms, complete the employee signature 
+                section below and contact your supervisor immediately for further direction. <b> If you
+                are not experiencing any of the symptoms above, complete the employee signature
+                section below, proceed to your worksite and provide this signed form to your supervisor/manager
+                upon arrival</b>
+            </p> 
+
+            By signing below, I certify that my response above are accurate to the best of my knowledge.
+            
+            <label>  Employee signature :  
+            <input type="text" text={this.state.text} onChange={this.handleChange} />
+            </label>
+            <label>  Date :  
+            <input type="number" number={this.state.number} onChange={this.handleChange} />
+            </label>
+            <label>  Department/Worksite Location :  
+            <input type="text" text={this.state.text} onChange={this.handleChange} />
+            </label>
+
          
          <input type="submit" value="Submit" />
        
