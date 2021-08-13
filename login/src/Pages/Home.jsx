640755d8f9969e0bce583e5a811cc4372a78b2e3
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-13 09:59:48 -0700

    added access to offical governemnt vaccine card

--- login/src/Pages/Home.jsx
+++ login/src/Pages/Home.jsx
@@ -3,8 +3,22 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className='home'>
-    <h1> WELCOME TO THE MONTEREY COUNTY HOMEPAGE </h1>
+    <div>
+    <h1 className="homeTitle"> WELCOME TO THE MONTEREY COUNTY HOMEPAGE </h1>
+    <p className="homeBody">
+      Welcome to the County of Monterey Homepage.  
+    </p>
+
+    <p className="homeBody">
+      Access the hamburger view to navigate the site.
+       Use the link below to access your offical government Covid Vaccine Card.  
+    </p>
+   
+
+    <a className="homeBody" href="https://myvaccinerecord.cdph.ca.gov/" rel="noreferrer">
+    <b> Access Your Digital Government Vaccine Card </b>
+    </a>
+    
     </div>
   );
 }
