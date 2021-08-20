0c9211426b91b06f4834e9bfa463c96d00fb2b55
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-20 15:12:40 -0700

    Edited the County Sites Page

--- login/src/Pages/CountySite.jsx
+++ login/src/Pages/CountySite.jsx
@@ -7,34 +7,6 @@ function CountySite() {
     <div className='countySiteBody'>
 
       <h1> County Site </h1>
-{/* 
-      <a href="https://www.co.monterey.ca.us/home" rel="noreferrer">
-        <b> County of Monterey Home </b>
-      </a>
-
-      <br />
-
-      <a href="https://arcg.is/Hfefy" rel="noreferrer">
-        <b> Emergency Shelters </b>
-      </a>
-
-      <br />
-
-      <a href="https://www.governmentjobs.com/careers/montereycounty" rel="noreferrer">
-        <b> Monterey County Career Opportunities </b>
-      </a>
-
-      <br />
-
-      <a href="https://www.caljobs.ca.gov/vosnet/Default.aspx" rel="noreferrer">
-        <b> Cal Jobs Emploment Opportunities </b>
-      </a>
-
-      <br />
-
-      <a href="http://montereyco.maps.arcgis.com/apps/Shortlist/index.html?appid=d24344d65e7443889cf6cae29f95bd75" rel="noreferrer">
-        <b> Special Events In Monterey County </b>
-      </a> */}
 
       <Row xs={1} md={2} className="g-4">
 
@@ -43,20 +15,20 @@ function CountySite() {
             <Card.Body>
               <Card.Title>County of Monterey Home</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.
+                Quick Access to Monterey County Recources And COIVID-19 Information
               </Card.Text>
+              <Button href="https://www.co.monterey.ca.us/home" variant="primary">Click Here</Button>
             </Card.Body>
           </Card>
-        </Col>
+        </Col> 
         <Col>
           <Card>
             <Card.Body>
               <Card.Title>Emergency Shelters</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.
+                See Emergenacy Shelters Currently Open In Monterey County
               </Card.Text>
+              <Button href="https://arcg.is/Hfefy" variant="primary">Click Here</Button>
             </Card.Body>
           </Card>
         </Col>
@@ -66,35 +38,32 @@ function CountySite() {
             <Card.Body>
               <Card.Title>Monterey County Career Opportunities</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.
+                Find Local Job Opportunities In The Monterey County
               </Card.Text>
+              <Button href="https://www.governmentjobs.com/careers/montereycounty" variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card>
             <Card.Body>
-              <Card.Title>Cal JObs Employment Oppotunities</Card.Title>
+              <Card.Title>Cal Jobs Employment Oppotunities</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit longer.
+                Find Exciting Career Opportunities Across California
               </Card.Text>
+              <Button href="https://www.caljobs.ca.gov/vosnet/Default.aspx" variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-
-      <Card>
-        <Card.Header>
-
-        </Card.Header>
+     
+     <Card className="special">
         <Card.Body>
-          <Card.Title>Special Events in Monterey County </Card.Title>
+          <Card.Title>Special Events in Monterey County</Card.Title>
           <Card.Text>
-            With supporting text below as a natural lead-in to additional content.
+            Follow This Link To See The Latest Events Happening In Montrey County
           </Card.Text>
-          <Button href="/" variant="primary">Go somewhere</Button>
+          <Button href="http://montereyco.maps.arcgis.com/apps/Shortlist/index.html?appid=d24344d65e7443889cf6cae29f95bd75" variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
 
