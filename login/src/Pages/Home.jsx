253b7e24d4a8499af2faa7d296d08c22b13c1d4a
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-20 16:31:46 -0700

    Navbar Fixes

--- login/src/Pages/Home.jsx
+++ login/src/Pages/Home.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 import './Home.css';
 import {Button } from 'react-bootstrap';
 
-
 function Home() {
   return (
     <div className="homeBody">
       <h1 className="homeTitle"> WELCOME TO THE MONTEREY COUNTY HOMEPAGE </h1>
-
-      <p>
-        Use the link below to access your offical government Covid Vaccine Card.
-      </p>
-
+      <p> Use the link below to access your offical government Covid Vaccine Card.</p>
       <Button href="https://myvaccinerecord.cdph.ca.gov/" variant="dark">Click Here To Access Your Offical Digtal Vaccine Card</Button>
-
-
     </div>
-
-    
   );
 }
 
