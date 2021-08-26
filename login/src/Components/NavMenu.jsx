180dd0d81eff97d7286e3ccbfa64029be35f3024
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-26 15:08:54 -0700

    Added corrections to App, Might add 4th page to applicaiton

--- login/src/Components/NavMenu.jsx
+++ login/src/Components/NavMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import Logo from './Images/Logo1.png';
 
 function NavMenu() {
 
@@ -7,14 +8,16 @@ function NavMenu() {
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">Monterey County Home</Navbar.Brand>
+                    <img src={Logo} alt="countyLogo"/>
+                    <Navbar.Brand href="/">Monterey County Digital Portal</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="/SelfCheck">COVID-19 Self Check In </Nav.Link>
-                            <Nav.Link href="/CountySites">Monterey County Recources</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/">Government Vaccine Link</Nav.Link>
+                                <Nav.Link href="/SelfCheck">COVID-19 Self Check In </Nav.Link>
+                                <Nav.Link href="/CountySites">Monterey County Recources</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
