6df8811bc3d6f2953424c0588e2a22a077e59b61
Christian Maldonado <chris.maldonado99@gmail.com>
2021-08-31 16:37:55 -0700

    Corrections and Footer Testing

--- login/src/Pages/Footer.jsx
+++ login/src/Pages/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className="FooterLinks">
             <Nav defaultActiveKey="/home" as="ul">
                 <Nav.Item as="li">
-                    <Nav.Link href="/home">Potral Home </Nav.Link>
-                    <Nav.Link href="/home">Digital Vaccine</Nav.Link>
+                    <Nav.Link href="/home">Poral Home </Nav.Link>
+                    <Nav.Link href="/home">Ditgital Vaccine</Nav.Link>
                     <Nav.Link href="/home">Active</Nav.Link>
                 </Nav.Item>
                 <Nav.Item as="li">
@@ -19,6 +19,40 @@ const Footer = () => {
                     <Nav.Link eventKey="link-2">Resources</Nav.Link>
                 </Nav.Item>
             </Nav>
+
+<Nav className="justify-content-center" activeKey="/" variant="tabs">
+    <Nav.Item>
+      <Nav.Link href="/">Active</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
+  <p className="text-center mt-4 mb-4">Or right-aligned</p>
+  <Nav className="justify-content-end" activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Active</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
         </div>
     );
 };
